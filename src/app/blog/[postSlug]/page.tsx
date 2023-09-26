5d3bcab13341749122562dd5a3e873f5abd821e8
Marco Pacifico <marco.pacifico@gmail.com>
2023-09-25 22:00:23 -0400

    M-20

--- src/app/blog/[postSlug]/page.tsx
+++ src/app/blog/[postSlug]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 export default function BlogPostPage({
   params,
 }: {
@@ -5,6 +6,7 @@ export default function BlogPostPage({
 }) {
   return (
     <div>
+      <Link href="/blog">Back</Link>
       <h1>Blog Post Page</h1>
       <p>Blog post content for {params.postSlug}</p>
     </div>
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,22 @@
+import Link from "next/link";
 export default function BlogPage() {
   return (
-    <div>
+    <main>
       <h1>Blog Page</h1>
       <p>List of blog posts</p>
-    </div>
-  )
+      <ul>
+        {POSTS.map((post) => (
+          <li key={post.slug}>
+            <Link href={`/blog/${post.slug}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
+
+const POSTS = [
+  { slug: "first-post", title: "First Post" },
+  { slug: "second-post", title: "Second Post" },
+  { slug: "third-post", title: "Third Post" },
+];
